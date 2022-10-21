b096b9f726b37ff2bdf853ff66cbb2a4b93ae67f
Aysun A. , BA AMBCS <65825819+london-ay@users.noreply.github.com>
2022-10-22 00:35:21 +0100

    added files

---
+++ BlogSite2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "home";
+        }
+
+        public string Blog() 
+        {
+            return "Teresa May is back!";
+        }
+    }
+}
---
+++ BlogSite2/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite2.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Blog()
+        {
+            return "Teresa May is back!";
+        }
+    }
+}
